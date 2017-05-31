d7a8532b4dfa8fdaacaf0b981754092160d29748
Matthias Baumgart <matthias@briefhansa.de>
2017-05-31 09:02:19 +0200

    Refactor extract product

--- src/app/product/product-overview/product-overview.component.ts
+++ src/app/product/product-overview/product-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../products.types';
 
 @Component({
   selector: 'app-product-overview',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductOverviewComponent implements OnInit {
 
-  products = ['1984', 'Das Leben des Brian', 'Pulp Fiction'];
+  products: Product[] = [{name: '1984'}, {name: 'Das Leben des Brian'}, {name: 'Pulp Fiction'}];
 
   constructor() { }
 
